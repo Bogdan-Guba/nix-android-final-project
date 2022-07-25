2439099e4de23791a9573620b375489c4f1323e6
“Bogdan <guba.bogd@gmail.com>
2022-07-25 11:12:18 +0300

    Task2

--- app/src/androidTest/java/com/nix/summer/final_project/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/nix/summer/final_project/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.nix_android_final_project
+package com.nix.summer.final_project
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.nix_android_final_project", appContext.packageName)
+        assertEquals("com.nix.summer.final_project", appContext.packageName)
     }
 }
---
+++ app/src/main/java/com/nix/summer/final_project/Model.kt
@@ -0,0 +1,115 @@
+package com.nix.summer.final_project
+
+//  Recipe coffee.
+enum class Coffee(val water: Int, val coffeeBeans: Int, val milk: Int, val money: Int) {
+    ESPRESSO(250, 16, 0, 4),
+    LATTE(350, 20, 75, 7),
+    CAPPUCCINO(200, 12, 100, 6);
+}
+
+//  Order - Types of coffee.
+data class Order(val choice: String)
+
+//  Resources - Adding resources or withdrawing cash.
+data class Resources(
+    val water: Int = 400,
+    val milk: Int = 540,
+    val coffeeBeans: Int = 120,
+    val cups: Int = 9,
+    val money: Int = 550) {
+    data class ChangeRes(
+        var water: Int = 0,
+        var milk: Int = 0,
+        var coffeeBeans: Int = 0,
+        var cups: Int = 0,
+        var money: Int= 0
+    )
+}
+
+//  Response - date class text outputs.
+data class Response(val notify: String)
+
+//  The class responsible for the logic of the program.
+class Model {
+    //  Initialization of date classes.
+    private val res = Resources.ChangeRes()
+
+    // Setting default values.
+    fun setData() {
+        val resources = Resources()
+        res.water = resources.water
+        res.coffeeBeans = resources.coffeeBeans
+        res.milk = resources.milk
+        res.cups = resources.cups
+        res.money = resources.money
+    }
+
+    //  Forming the output of information about the remains in the coffee machine.
+    fun coffeeMachineRemaining (): String {
+        return "${res.water} of water\n" +
+                "${res.milk} of milk\n" +
+                "${res.coffeeBeans} of coffee beans\n" +
+                "${res.cups} of disposable cups\n" +
+                "${res.money} of money"
+    }
+
+    //  Buying instructions.
+    fun buy(order: Order): Response {
+        var response = Response("")
+        if (order.choice == "1" || order.choice == "2" || order.choice == "3" || order.choice == "back") {
+            when (order.choice) {
+                "1" -> {
+                    response = makeCoffee(Coffee.ESPRESSO)
+                }
+                "2" -> {
+                    response = makeCoffee(Coffee.LATTE)
+                }
+                "3" -> {
+                    response = makeCoffee(Coffee.CAPPUCCINO)
+                }
+                "back" -> {
+                    return response
+                }
+            }
+        } else {
+            response = Response("Invalid option.")
+        }
+        return response
+    }
+
+    //  Logic for calculating resources and displaying information about them.
+    private fun makeCoffee(coffeeType: Coffee): Response {
+        if (coffeeType.water > res.water) {
+            return Response("Sorry, not enough water!")
+        } else if (coffeeType.milk > res.milk) {
+            return Response("Sorry, not enough milk!")
+        } else if (coffeeType.coffeeBeans > res.coffeeBeans) {
+            return Response("Sorry, not enough coffee beans!")
+        } else if (res.cups < 1) {
+            return Response("Sorry, not enough disposable cups!")
+        }  else {
+            res.cups -= 1
+            res.water -= coffeeType.water
+            res.coffeeBeans -= coffeeType.coffeeBeans
+            res.milk -= coffeeType.milk
+            res.money += coffeeType.money
+            return Response("I have enough resources, making you a coffee!")
+        }
+    }
+
+    // Filling Resources.
+    fun fill(resources: Resources.ChangeRes): Response {
+        res.water += resources.water
+        res.milk += resources.milk
+        res.coffeeBeans += resources.coffeeBeans
+        res.cups += resources.cups
+        return Response("Resources replenished.")
+    }
+
+    //  Issuance of money.
+    fun take(): Response {
+        val response = Response("I gave you ${res.money}")
+        res.money = 0
+        return response
+    }
+}
